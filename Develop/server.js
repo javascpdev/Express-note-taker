fef27cba3ad232498b202a58807c6687c3e2d3c2
javascpdev <kingpower1990115@gmail.com>
2021-09-25 11:42:59 -0400

    commeted out everything that had the server not running

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,20 @@
 //Boiler plate
 const express = require("express");
 const path = require("path");
-const { clog } = require("./middleware/clog");
-const api = require("./routes/index.js");
+// const { clog } = require("./middleware/clog");
+// const api = require("./Develop/routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 //Import custom middleware, "clog" I am not sure if I will need this but we used it on the mini project delete if not needed.
-app.use(clog);
+// app.use(clog);
 
 //Middleware for parsing JSON and urlencoded form data.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("api", api);
+// app.use("api", api);
 
 app.use(express.static("public"));
 
