f782aaa3afe69330ebbd86c23bb2d31ac55e27bc
javascpdev <kingpower1990115@gmail.com>
2021-09-25 20:42:48 -0400

    working app

--- Develop/server.js
+++ Develop/server.js
@@ -1,39 +1,23 @@
-//Boiler plate
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-// const { clog } = require("./middleware/clog");
-// const api = require("./Develop/routes/index.js");
+const routes = require("./routes/notesRoute");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-
-//Import custom middleware, "clog" I am not sure if I will need this but we used it on the mini project delete if not needed.
-// app.use(clog);
-
-//Middleware for parsing JSON and urlencoded form data.
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use("api", api);
-
 app.use(express.static("public"));
+app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Get route for homepage.
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 
-//Get route for notes page.
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes"));
-});
+ app.get("/", (req, res) =>
+   res.sendFile(path.join(__dirname, "./routes/notesRoute"))
+ );
 
-//Wildcard route for 404.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/404.html"));
-});
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 
-//Listing on port.
-app.listen(PORT, () => {
-  console.log(`App is listening at http://localhost${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
