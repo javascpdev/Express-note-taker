4decb4e23a2b63c09514990c1c5011260189eb80
javascpdev <kingpower1990115@gmail.com>
2021-10-13 12:17:26 -0400

    got the note to show up in the console still trying to figure out how to add the not to the page

--- routes/notes.js
+++ routes/notes.js
@@ -1,46 +1,24 @@
+const app = require("express").Router();
+let dataBase = require("../db/db.json");
+const fs = require("fs");
 
-
-// -------============--------not my code
-
-const notes = require("express").Router();
-// const { readFromFile, readAndAppend, readAndDelete } = require("../helpers/fsUtils");
-// const uuid = require("../helpers/uuid");
-
-// Returns the db.json file
-notes.get("/", (req, res) => {
-    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+app.get("/notes", (req, res) => {
+  res.json(dataBase);
 });
 
-// Create a new note and adds it to the existing db.json file
-notes.post("/", (req, res) => {
-    const { title, text } = req.body;
+app.post("/notes", (req, res) => {
+  let note = req.body;
+  console.log(note);
+  dataBase.push(note);
 
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            id: uuid(),
-        };
-
-        readAndAppend(newNote, "./db/db.json");
-        res.json("Note has been added successfully");
-    } else {
-        res.error("Error occured while adding note");
+  fs.writeFile(
+    __dirname + "/../db/db.json",
+    JSON.stringify(dataBase),
+    (error) => {
+      if (error) throw error;
     }
+  );
+  res.end();
 });
 
-// Handles the delete method by passing the id parameter to the readAndDelete method
-notes.delete('/:id', (req, res) => {
-
-    const requestedId = req.params.id;
-
-    if (requestedId) {
-
-        readAndDelete(requestedId, "./db/db.json");
-        res.json("Note has been deleted successfully");
-    } else {
-        res.error("Error occured whiled deleting note");
-    }
-});
-
-module.exports = notes;
+module.exports = app;
